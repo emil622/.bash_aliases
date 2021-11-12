74853de1b421404c9e77cf0db8a95586656f8f33
emilmicu <emicu@grubhub.com>
2021-11-12 22:24:10 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bash_profile_file=~/.bash_profile
+
+echo >> ${bash_profile_file}
+echo >> ${bash_profile_file}
+echo '#### Developer aliases configuration ####' >> ${bash_profile_file}
+echo >> ${bash_profile_file}
+echo 'source ~/.bash_aliases/.bash_profile_aliases'  >> ${bash_profile_file}
